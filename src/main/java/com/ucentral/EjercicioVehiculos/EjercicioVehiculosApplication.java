1f9cda0b37171fc396c15370eef8d91a3460dba0
Juan Romero <jromeroc21@ucentral.edu.co>
2025-03-28 20:22:23 -0500

    Initial Commit

---
+++ src/main/java/com/ucentral/EjercicioVehiculos/EjercicioVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.ucentral.EjercicioVehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucentral/EjercicioVehiculos/EjercicioVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucentral.EjercicioVehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
